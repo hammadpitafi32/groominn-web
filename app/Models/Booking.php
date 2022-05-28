4988cce9fdbaf744bb1b2429119595f506721d4e
Ausaf Saqib <ausaf@pkteam.com>
2022-05-28 22:58:13 +0500

    booking api done

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -10,4 +10,13 @@ class Booking extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    protected $fillable = [
+        'booking_no','user_id','user_business_id','total_duration','date','estimated_time','payment_type','charges'
+    ];
+
+    public function booking_services()
+    {
+        return $this->hasMany(BookingService::class);  
+    }
 }
--- app/Models/BookingService.php
+++ app/Models/BookingService.php
@@ -10,4 +10,12 @@ class BookingService extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    protected $fillable = [
+        'booking_id','user_business_category_service_id','duration','charges','date','type'
+    ];
+    public function booking()
+    {
+        return $this->belongsTo(Booking::class);
+    }
 }
